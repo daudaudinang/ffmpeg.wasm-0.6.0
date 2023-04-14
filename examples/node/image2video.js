297fa39b2d73c7e87ffcbb5c03632cc2884c6d31
huynq <huynguyenquang@vccorp.vn>
2023-04-14 11:28:28 +0700

    Update 0.7.0

---
+++ examples/node/image2video.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { createWorker } = require('../../src');
+
+const worker = createWorker({
+  progress: (p) => console.log(p),
+});
+
+(async () => {
+  console.log('Loading ffmpeg-core.js');
+  await worker.load();
+  console.log('Loading data');
+  await worker.write('audio.ogg', '../../tests/assets/triangle/audio.ogg');
+  for (let i = 0; i < 60; i += 1) {
+    const num = `00${i}`.slice(-3);
+    await worker.write(`tmp.${num}.png`, `../../tests/assets/triangle/tmp.${num}.png`);
+  }
+  console.log('Start transcoding');
+  await worker.run('-framerate 30 -pattern_type glob -i /data/*.png -i /data/audio.ogg -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p out.mp4', { output: 'out.mp4' });
+  const { data } = await worker.read('out.mp4');
+  console.log('Complete transcoding');
+  await worker.remove('audio.ogg');
+  for (let i = 0; i < 60; i += 1) {
+    const num = `00${i}`.slice(-3);
+    await worker.remove(`tmp.${num}.png`);
+  }
+  fs.writeFileSync('out.mp4', Buffer.from(data));
+  await worker.terminate();
+})();
---
+++ tests/ffmpeg.test.js
@@ -0,0 +1,20 @@
+const { createWorker } = FFmpeg;
+const worker = createWorker(OPTIONS);
+
+before(async function cb() {
+  this.timeout(0);
+  await worker.load();
+});
+
+describe('transcode()', () => {
+  describe('should transcode different format', () => {
+    ['flame.avi'].forEach((name) => (
+      it(`transcode ${name}`, async () => {
+        await worker.write(name, `${BASE_URL}/${name}`);
+        await worker.transcode(name, 'output.mp4');
+        const { data } = await worker.read('output.mp4');
+        expect(data.length).to.be(FLAME_MP4_LENGTH);
+      }).timeout(TIMEOUT)
+    ));
+  });
+});
